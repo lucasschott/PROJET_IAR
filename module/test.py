a4e38ef256cdae6b9dfccf64e4c3c490e7327cce
Hector Roussille <hector.roussille@gmail.com>
2019-12-03 00:32:04 +0100

    more work on test.py trying to reduce variance

--- module/test.py
+++ module/test.py
@@ -17,7 +17,7 @@ num_predators = 1
 env_x = 512
 env_y = 512
 eat_distance = 15
-confusion = True
+confusion = False
 timesteps = 2000
 
 pred_genotype = list(np.random.rand(PRED_NETWORK_SIZE))
@@ -69,7 +69,7 @@ def pred_eval(pred_indiv,preys_population,confusion):
     for prey_indiv in preys_population:
         pred_fitness, prey_fitness = eval_(pred_indiv,prey_indiv,confusion)
         sum_pred_fitnesses += pred_fitness
-    return sum_pred_fitnesses
+    return sum_pred_fitnesses / len(preys_population)
 
 
 def prey_eval(preds_population,prey_indiv,confusion):
@@ -77,11 +77,11 @@ def prey_eval(preds_population,prey_indiv,confusion):
     for pred_indiv in preds_population:
         pred_fitness, prey_fitness = eval_(pred_indiv,prey_indiv,confusion)
         sum_prey_fitnesses += prey_fitness
-    return sum_prey_fitnesses
+    return sum_prey_fitnesses / len(preds_population)
 
 def cmaes(nb_gen=15, confusion=True, display=True):
 
-    es_preds = cma.CMAEvolutionStrategy(pred_genotype, 1)
+    es_preds = cma.CMAEvolutionStrategy(pred_genotype, 0.6)
 
     survivorships = []
     swarm_densitys = []
@@ -91,13 +91,11 @@ def cmaes(nb_gen=15, confusion=True, display=True):
 
     for i in range(nb_gen):
         preds_population = es_preds.ask()
-        prey_genotype = list(np.random.rand(PREY_NETWORK_SIZE))
+        prey_genotype = np.random.rand(3, PREY_NETWORK_SIZE)
 
-        pred_eval_part=partial(pred_eval, preys_population=[prey_genotype], confusion=confusion)
-
-
+        pred_eval_part=partial(pred_eval, preys_population=prey_genotype, confusion=confusion)
         preds_fitnesses = pool.map(pred_eval_part, [pred_indiv for pred_indiv in preds_population])
-        #preds_fitnesses = [pred_eval(pred_indiv, [prey_genotype], confusion) for pred_indiv in preds_population]
+
         print("GENERATION {} : BEST = [{:1.2f}] AVERAGE = [{:1.2f}] WORST = [{:1.2f}]".format(i, -min(preds_fitnesses), -np.mean(preds_fitnesses), -max(preds_fitnesses)))
 
         es_preds.tell(preds_population, preds_fitnesses)
@@ -106,10 +104,9 @@ def cmaes(nb_gen=15, confusion=True, display=True):
 
         results = []
 
-        for _ in range(1):
-            prey_genotype = list(np.random.rand(PREY_NETWORK_SIZE))
+        for prey in prey_genotype:
             s.reset_population()
-            s.load_prey_genotype(list(prey_genotype))
+            s.load_prey_genotype(list(prey))
             s.load_predator_genotype(list(pred))
             results.append(s.run(timesteps))
 
@@ -131,7 +128,7 @@ def cmaes(nb_gen=15, confusion=True, display=True):
 if __name__ == "__main__":
 
     t1 = time.time()
-    survivorships, swarm_densitys, swarm_dispersions = cmaes(nb_gen=25,confusion=confusion)
+    survivorships, swarm_densitys, swarm_dispersions = cmaes(nb_gen=50,confusion=confusion)
     t2 = time.time()
 
     print("EVOLUTION LEARNING FINISHED IN : {} m {} s".format((t2 - t1) // 60, (t2 - t1) % 60))
