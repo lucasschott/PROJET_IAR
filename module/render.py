f4d74d2f377e8cf4f1eb8561b7091b51eec67ae6
Hector Roussille <hector.roussille@gmail.com>
2019-12-11 15:24:34 +0100

    Improved render script : now using step_hooks, displaying predator observations / predator field of view and predator direction, KEYUP and KEYDOWN can now be used to accelerate / decelerate the simulation by steps of 10 FPS

--- module/render.py
+++ module/render.py
@@ -58,17 +58,38 @@ s.load_predator_genotype(pred)
 successes, failures = pygame.init()
 print("{0} successes and {1} failures".format(successes, failures))
 
-screen = pygame.display.set_mode((args.env_x, args.env_y))
+screen = pygame.display.set_mode((args.env_x, args.env_y + 80))
 clock = pygame.time.Clock()
+
+pred_vision = pygame.Surface((400, 400))
+pred_vision.fill(WHITE)
+pred_vision.set_colorkey(WHITE)
+pygame.draw.circle(pred_vision, BLACK, (200, 200), 200, 1)
 
 prey_sprite = pygame.Surface((10, 10))
 prey_sprite.fill(WHITE)
+prey_sprite.set_colorkey(WHITE)
 pygame.draw.circle(prey_sprite, BLUE, (5, 5), 4)
 
-pred_sprite = pygame.Surface((15, 15))
+pred_sprite = pygame.Surface((400, 400))
 pred_sprite.fill(WHITE)
-pygame.draw.circle(pred_sprite, RED, (7, 7), 8)
-pygame.draw.line(pred_sprite, BLACK, (7, 7), (7, 0), 2)
+pred_sprite.set_colorkey(WHITE)
+pygame.draw.circle(pred_sprite, BLACK, (200, 200), 200, 1)
+pygame.draw.rect(pred_sprite, WHITE, (0, 0, 400, 200))
+pygame.draw.circle(pred_sprite, RED, (200, 200), 8)
+
+obs_bg = pygame.Surface((args.env_x, 80))
+obs_bg.fill(BLACK)
+
+obs_activated = pygame.Surface((20, 20))
+obs_activated.fill(BLACK)
+
+pygame.draw.circle(obs_activated, WHITE, (10, 10), 8)
+pygame.draw.circle(obs_activated, RED, (10, 10), 6)
+
+obs_idle = pygame.Surface((20, 20))
+obs_idle.fill(BLACK)
+pygame.draw.circle(obs_idle, WHITE, (10, 10), 8)
 
 FPS = args.fps
 
@@ -79,11 +100,20 @@ fit_pred = 0
 
 old = [pos[2] for pos in s.get_predators_pos()]
 
+pred_obs = None
+
+def pred_hook(observations):
+    global pred_obs
+    pred_obs = observations[0]
+
+def prey_hook(observations):
+    pass
+
 for timestep in range(args.timesteps):
     screen.fill(WHITE)
     clock.tick(FPS)
 
-    s.step()
+    s.step_hook(pred_hook, prey_hook)
     preys_pos = s.get_preys_pos()
     preds_pos = s.get_predators_pos()
 
@@ -94,18 +124,50 @@ for timestep in range(args.timesteps):
         count = len(preys_pos)
 
     for prey in preys_pos:
-        screen.blit(prey_sprite, (int(prey[0]), int(prey[1])))
+        screen.blit(prey_sprite, (int(prey[0]) - prey_sprite.get_width() / 2, int(prey[1]) - prey_sprite.get_height() / 2))
 
     for index, pred in enumerate(preds_pos):
-        if old[index] - pred[2] > 0:
-            sp = pygame.transform.rotate(pred_sprite, -1 * np.degrees(pred[2]))
+        sp = pygame.transform.rotate(pred_sprite, -1 *  np.degrees(pred[2]) + 90)
+        screen.blit(sp, (int(pred[0] - sp.get_width() / 2), int(pred[1]) - sp.get_height() / 2))
+        end_x = pred[0] + np.cos(pred[2]) * 200
+        end_y = pred[1] + np.sin(pred[2]) * 200
+
+        pygame.draw.line(screen, BLUE, (pred[0], pred[1]), (end_x, end_y), 1)
+
+        end_x = pred[0] + np.cos(pred[2] - np.pi / 2) * 200
+        end_y = pred[1] + np.sin(pred[2] - np.pi / 2) * 200
+
+        pygame.draw.line(screen, RED, (pred[0], pred[1]), (end_x, end_y), 1)
+
+        end_x = pred[0] + np.cos(pred[2] + np.pi / 2) * 200
+        end_y = pred[1] + np.sin(pred[2] + np.pi / 2) * 200
+
+        pygame.draw.line(screen, RED, (pred[0], pred[1]), (end_x, end_y), 1)
+
+    screen.blit(obs_bg, (0, args.env_y))
+
+    for index, obs in enumerate(pred_obs):
+
+        if obs == 1:
+            sprite = obs_activated
         else:
-            sp = pygame.transform.rotate(pred_sprite, np.degrees(pred[2]))
-        screen.blit(sp, (int(pred[0]), int(pred[1])))
+            sprite = obs_idle
+
+        screen.blit(sprite, ((args.env_x / args.input) * index + sprite.get_width() / 2, args.env_y + 40 - sprite.get_height() / 2))
+
+
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             quit()
+
+        if event.type ==  pygame.KEYDOWN:
+
+            if event.key == pygame.K_DOWN:
+                FPS = max(1, FPS - 10)
+
+            if event.key == pygame.K_UP:
+                FPS = FPS + 10
 
     pygame.display.update()  # Or pygame.display.flip()
 
