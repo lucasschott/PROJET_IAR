fix network weighting bug

visualisations/network/weighting_network.py
@@ -43,6 +43,9 @@ nn = np.load("best_prey.npy","r")
 read = open("prey_nn_tmp.svg","r")
 write = open("prey_nn_weight.svg","w")
 
+max = nn.max()
+min = nn.min()
+
 i=0
 j=0
 linear = 1
@@ -53,7 +56,14 @@ while True:
     if not re.search(r'^.*stroke-opacity: ([0-9|.]*);.*$', line):
         write.write(line)
     else:
-        new_line = re.sub(r'stroke-opacity: ([0-1]\.[0-9]*);', "stroke-opacity: {};".format(1/(1+np.exp(-nn[i]))), line)
+        opacity = np.abs(np.tanh(nn[i]/4))
+        color = int((1-opacity)*255)
+        new_line_tmp = re.sub(r'stroke-opacity: [0-1]\.[0-9]*;', "stroke-opacity: {};".format(opacity), line)
+        if nn[i] <= 0:
+            new_line = re.sub(r'stroke: rgb\(.*\);', "stroke: rgb(255,{},{});".format(color,color), new_line_tmp)
+        else:
+            new_line = re.sub(r'stroke: rgb\(.*\);', "stroke: rgb({},{},255);".format(color,color), new_line_tmp)
+
         write.write(new_line)
         i+=1
         j+=1
@@ -74,6 +84,9 @@ nn = np.load("best_pred.npy","r")
 read = open("pred_nn_tmp.svg","r")
 write = open("pred_nn_weight.svg","w")
 
+max = nn.max()
+min = nn.min()
+
 i=0
 j=0
 linear = 1
@@ -84,7 +97,14 @@ while True:
     if not re.search(r'^.*stroke-opacity: ([0-9|.]*);.*$', line):
         write.write(line)
     else:
-        new_line = re.sub(r'stroke-opacity: ([0-1]\.[0-9]*);', "stroke-opacity: {};".format(1/(1+np.exp(-nn[i]))), line)
+        opacity = np.abs(np.tanh(nn[i]/5))
+        color = int((1-opacity)*255)
+        new_line_tmp = re.sub(r'stroke-opacity: [0-1]\.[0-9]*;', "stroke-opacity: {};".format(opacity), line)
+        if nn[i] <= 0:
+            new_line = re.sub(r'stroke: rgb\(.*\);', "stroke: rgb(255,{},{});".format(color,color), new_line_tmp)
+        else:
+            new_line = re.sub(r'stroke: rgb\(.*\);', "stroke: rgb({},{},255);".format(color,color), new_line_tmp)
+
         write.write(new_line)
         i+=1
         j+=1